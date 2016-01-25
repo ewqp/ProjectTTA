fe0ea659f6d54fb54055e457234e72ede342fe71
canor6 <qwester1213@op.pl>
2016-01-25 03:24:46 +0100

    Add Library.Tests.Integration

---
+++ code/Library.Tests.Integration/Storage/BaseRepositoryTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+using NUnit.Framework;
+using Library.Storage;
+
+namespace Library.Tests.Integration.Storage
+{
+    public class BaseRepositoryTests
+    {
+        protected static Func<DbContext> DbContextFactory;
+
+        [TestFixtureSetUp]
+        public void FixtureSetUp()
+        {
+            DbContextFactory = () => new DbContext("IntegrationTestsConnectionString");
+            Database.SetInitializer(new DropCreateDatabaseAlways<DbContext>());
+            DbContextFactory().Database.Initialize(true);
+        }
+    }
+}
